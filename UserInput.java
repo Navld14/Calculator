1c59fb8e2ce6202af5e196d5f93351a899e7f2ff
Navld14 <naveen.ladha.btech2023@sitpune.edu.in>
2025-02-11 10:57:10 +0530

    Adding switch cases for subtraction and multiplication

--- UserInput.java
+++ UserInput.java
@@ -1,6 +1,16 @@
-//Creating method handleOperations
+//UserInput.java
+import java.util.* ;
+
+public class UserInput{
+  private Scanner scan;
+  private final Scanner scan;
+
+  public UserInput(Scanner scan) {
+        this.scan = scan;
+    }
+
+  //Creating method handleOperations
   public void handleOperation(int choice, Calculator calculator) {
-  
     switch (choice) {
       case 1 -> {
         System.out.println("Enter two numbers:");
@@ -8,7 +18,18 @@
         double b = scan.nextDouble();
         System.out.println("Result: " + calculator.add(a, b));
       }
-        
+      case 2 -> {
+        System.out.println("Enter two numbers:");
+        double a = scan.nextDouble();
+        double b = scan.nextDouble();
+        System.out.println("Result: " + calculator.subtract(a, b));
+      }
+      case 3 -> {
+        System.out.println("Enter two numbers:");
+        double a = scan.nextDouble();
+        double b = scan.nextDouble();
+        System.out.println("Result: " + calculator.multiply(a, b));
+      }
+
     }
   }
-}
