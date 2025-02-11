ea413eefcb96c3461a855f7c1d24a4a85d1780a9
Navld14 <naveen.ladha.btech2023@sitpune.edu.in>
2025-02-11 10:55:46 +0530

    Adding Switch case for addition

--- UserInput.java
+++ UserInput.java
@@ -1,12 +1,14 @@
-public class UserInput{
-  private Scanner scan;
-  private final Scanner scan;
-  
-  public UserInput(Scanner scan) {
-        this.scan = scan;
-    }
-  //Creating method handleOperations
+//Creating method handleOperations
   public void handleOperation(int choice, Calculator calculator) {
-
+  
+    switch (choice) {
+      case 1 -> {
+        System.out.println("Enter two numbers:");
+        double a = scan.nextDouble();
+        double b = scan.nextDouble();
+        System.out.println("Result: " + calculator.add(a, b));
+      }
+        
+    }
   }
 }
